3e70952824aae4301b7ab15db6e780a9c747615e
Wiredu Clement <wireduclement2334@gmail.com>
2025-05-24 18:21:15 +0000

    implement dashboard

--- main.py
+++ main.py
@@ -14,24 +14,25 @@ app.secret_key = os.environ.get("SECRET_KEY")
 
 db = Database("localhost", "root", "", "voting_db")
 
+
 def login_required(f):
     @wraps(f)
     def decorated_function(*args, **kwargs):
         if not session.get("logged_in"):
-            flash("You must log in to access this page.", "warning")
+            flash("You must log in to access that page.", "info")
             return redirect(url_for("login"))
         return f(*args, **kwargs)
     return decorated_function
 
 
-@app.route("/")
-def homepage():
-    return render_template("index.html")  
+class HomeView(MethodView):
+    def get(self):
+        return render_template("index.html") 
+    
 
-
-@app.route("/vote")
-def vote():
-    return render_template("vote.html")
+class VoteView(MethodView):
+    def get(self):
+        return render_template("vote.html")
 
 
 class LoginView(MethodView):
@@ -60,16 +61,54 @@ class LoginView(MethodView):
         return render_template("admin.html")
 
 
-@app.route("/admin/dashboard")
-def dashboard():
-    return render_template("dashboard.html")
+class DashboardView(MethodView):
+    decorators = [login_required] 
+    def get(self):
+        return render_template("dashboard.html")
+    
+
+class CodeView(MethodView):
+    decorators = [login_required]
+    def get(self):
+        return render_template("vote_codes.html")
+    
+
+class PositionsView(MethodView):
+    decorators = [login_required]
+    def get(self):
+        return render_template("positions.html")
+    
+
+class CandidatesView(MethodView):
+    decorators = [login_required]
+    def get(self):
+        return render_template("candidates.html")
+    
+
+class ResultsView(MethodView):
+    decorators = [login_required]
+    def get(self):
+        return render_template("results.html")
 
 
+@app.route("/logout")
+@login_required
+def logout():
+    session.pop("logged_in", None)
+    flash("You have been logged out.", "success")
+    return redirect(url_for("login"))
 
 
 
 # registers
+app.add_url_rule("/", view_func=HomeView.as_view("index"))
+app.add_url_rule("/vote", view_func=VoteView.as_view("vote"))
 app.add_url_rule("/admin/login", view_func=LoginView.as_view("login"))
+app.add_url_rule("/admin/dashboard", view_func=DashboardView.as_view("dashboard"))
+app.add_url_rule("/admin/generate-codes", view_func=CodeView.as_view("generate_codes"))
+app.add_url_rule("/admin/positions", view_func=PositionsView.as_view("positions"))
+app.add_url_rule("/admin/candidates", view_func=CandidatesView.as_view("candidates"))
+app.add_url_rule("/admin/results", view_func=ResultsView.as_view("results"))
 
 
 if __name__ == "__main__":
